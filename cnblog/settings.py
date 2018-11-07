61b5ef4c7d7826cc2636a85aeb6b7e4012a68406
huangjx <trustfor@sina.cn>
2018-11-07 21:11:26 +0800

    数据库连接信息   orm模型

--- cnblog/settings.py
+++ cnblog/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'cnblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'cnblog',# 要连接的数据库，连接前需要创建好
+        'USER':'root',# 连接数据库的用户名
+        'PASSWORD':'831015', # 连接数据库的密码
+        'HOST':'127.0.0.1',       # 连接主机，默认本级
+        'PORT':3306 #  端口 默认3306
     }
 }
+
 
 
 # Password validation
