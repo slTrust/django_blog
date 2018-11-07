3bd4cf7e338dd3f515b88e8a2f62fe01813a625c
huangjx <trustfor@sina.cn>
2018-11-07 23:05:44 +0800

    登陆验证 创建超级管理员python3 manage.py createsuperuser

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 from django.http import JsonResponse
+from django.contrib import auth
 
 def login(request):
-
+    """
+        登录视图函数:
+           get请求响应页面
+           post(Ajax)请求响应字典
+        :param request:
+        :return:
+    """
     if request.method=='POST':
         response = {"user":None,"msg":None}
 
@@ -14,11 +21,16 @@ def login(request):
 
         # 校验验证码 验证码存在session里  而不是 global里因为 另一个用户访问的时候 就变了
         valid_code_str = request.session.get('valid_code_str')
-        if valid_code.upper()==valid_code_str.upper():
+        if valid_code.upper() == valid_code_str.upper():
+            user = auth.authenticate(username=user, password=pwd)
+            if user:
+                auth.login(request, user)  # request.user== 当前登录对象
+                response["user"] = user.username
+            else:
+                response["msg"] = "用户名或者密码错误!"
 
-            pass
         else:
-            response['msg']='valid code error'
+            response["msg"] = "验证码错误!"
 
         return JsonResponse(response)
 
@@ -108,3 +120,5 @@ def get_validCode_img(request):
 
     return HttpResponse(data)
 
+def index(request):
+    return render(request,'index.html')
