040f7d1bfd9cd1878d8ba372bfd3ad2bb5a92961
huangjx <trustfor@sina.cn>
2018-11-18 21:01:00 +0800

    admin管理models进行数据录入

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# admin的使用方式：
+'''
+在这里进行注册  就可以在后台页面进行数据录入
+'''
+
+from blog import models
+admin.site.register(models.UserInfo)
+admin.site.register(models.Blog)
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Article)
+admin.site.register(models.ArticleUpDown)
+admin.site.register(models.Article2Tag)
+admin.site.register(models.Comment)
